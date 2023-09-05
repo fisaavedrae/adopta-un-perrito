9d9eee894c33261c0aad99f708cbf34d18c7420e
Felipe Saavedra <fisaavedrae@gmail.com>
2023-09-05 18:32:53 -0300

    fotos

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,24 @@ function App() {
         <Header titulo="Adopta un perrito" /> { /* Uso componente Header */}
         <div class="galeria">
           { /* Uso de los componentes Card */}
-          <Card urlImagen="./src/assets/images/beagle.jpg" nombre="Bartolo" descripcion="Lleno de energía y listo para jugar. !Dale a Bartolo el hogar amoroso que se merece!"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2023/05/20/16/48/dog-8006839_1280.jpg" nombre="Bartolo" descripcion="Lleno de energía y listo para jugar. !Dale a Bartolo el hogar amoroso que se merece!"
             badge="Beagle" colorBadge="success" />
-          <Card urlImagen="./src/assets/images/husky.jpg" nombre="Messi"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_1280.jpg" nombre="Messi"
             descripcion="Es jueguetón, amigable y se lleva bien con niños y otros animales. !Haz de Messi parte de tu familia hoy mismo!" badge="Husky" colorBadge="danger" />
-          <Card urlImagen="./src/assets/images/bulldog.jpg" nombre="Gohan"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2020/07/20/06/42/english-bulldog-5422018_1280.jpg" nombre="Gohan"
             descripcion="Un perrito de tamaño mediano que se adapta a cualquier ambiente. !Dale a Gohan el hogar amoroso que se merece!"
             badge="Bulldog" colorBadge="warning" />
-          <Card urlImagen="./src/assets/images/dalmatia.jpg" nombre="Princesa"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2021/05/09/10/51/dalmatian-6240486_1280.jpg" nombre="Princesa"
             descripcion="Es una compañera muy amigable. !Dale a Princesa el hogar amoroso que se merece!"
             badge="Dalmata" colorBadge="primary" />
-          <Card urlImagen="./src/assets/images/labrador.jpg" nombre="Ronaldo"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" nombre="Ronaldo"
             descripcion="Un gran amigo y compañero. !Dale a Ronaldo el hogar amoroso que se merece!"
             badge="Labrador" colorBadge="warning" />
-          <Card urlImagen="./src/assets/images/poodle.jpg" nombre="Gaston"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2022/07/12/17/12/dog-7317820_1280.jpg  " nombre="Gaston"
             descripcion="Fiel y serio, listo para jugar. !Dale a Gaston el hogar amoroso que se merece!"
             badge="Poodle" colorBadge="success" />
-          <Card urlImagen="./src/assets/images/yorkshire.jpg" nombre="Bartolo"
-            descripcion="Lleno de energía y listo para jugar. !Dale a Bartolo el hogar amoroso que se merece!"
+          <Card urlImagen="https://cdn.pixabay.com/photo/2020/01/21/16/26/yorkshire-terrier-4783327_1280.jpg" nombre="Yorki"
+            descripcion="Regalon, amigable y muy cariñoso. !Dale a Bartolo el hogar amoroso que se merece!"
             badge="Yorkshire" colorBadge="secondary" />
         </div>
         <Footer /> { /* Uso componente Footer */}
